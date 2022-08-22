9f51ce8692b69328335cdaea3719e1ff21853316
pranaysood24 <pranay.sd@gmail.com>
2022-08-22 10:03:56 +0000

    schema has code challenges

--- schema.sql
+++ schema.sql
@@ -4,4 +4,10 @@ CREATE TABLE users (
 );
 
 INSERT INTO users(email) VALUES
-('amy.sd@yahoo.com'), ('pranay.sd@gmail.com')
+('amy.sd@yahoo.com'), ('pranay.sd@gmail.com');
+
+SELECT 
+DATE_FORMAT(created_at, "%M %D %Y") AS earliest_date
+FROM users
+ORDER BY earliest_date
+LIMIT 1;
