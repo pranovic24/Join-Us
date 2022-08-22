3f4058179cb62112bfce892588b79101ebc2ff97
pranaysood24 <pranay.sd@gmail.com>
2022-08-22 09:16:30 +0000

    connection to mysql and connection query

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const { faker } = require('@faker-js/faker');
+var mysql = require('mysql');
+ 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'join_us'
+});
+
+var q = 'SELECT CURTIME() AS time, CURDATE() AS date, NOW() AS now';
+
+connection.query(q, function (error, results, fields) {
+   if (error) throw error;
+	console.log(results[0].time);
+	console.log(results[0].date);
+	console.log(results[0].now);
+});
+
+connection.end();
 
 // console.log(faker.internet.email());
 // console.log(faker.date.past());
@@ -7,12 +25,12 @@ const { faker } = require('@faker-js/faker');
 // console.log(faker.address.streetAddress());
 // console.log(faker.address.city());
 
-function generateAddress() {
-	console.log(faker.address.streetAddress());
-	console.log(faker.address.city());
-	console.log(faker.address.state());
-}
+// function generateAddress() {
+// 	console.log(faker.address.streetAddress());
+// 	console.log(faker.address.city());
+// 	console.log(faker.address.state());
+// }
 
-generateAddress();
-generateAddress();
-generateAddress();
+// generateAddress();
+// generateAddress();
+// generateAddress();
