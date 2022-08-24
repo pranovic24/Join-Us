e3fa20d2aaa29fd31a2bfe7b7f92be150fbcc813
pranaysood24 <pranay.sd@gmail.com>
2022-08-24 14:58:49 +0000

    schema has queries for Users Exercise

--- schema.sql
+++ schema.sql
@@ -7,7 +7,35 @@ INSERT INTO users(email) VALUES
 ('amy.sd@yahoo.com'), ('pranay.sd@gmail.com');
 
 SELECT 
-DATE_FORMAT(created_at, "%M %D %Y") AS earliest_date
+	DATE_FORMAT(MIN(created_at), "%M %D %Y") AS earliest_date
+FROM users;
+
+SELECT 
+	email,
+    created_at
 FROM users
-ORDER BY earliest_date
-LIMIT 1;
+WHERE created_at = (SELECT MIN(created_at) FROM users);
+
+SELECT
+    MONTHNAME(created_at) AS month,
+    COUNT(*) AS count
+FROM users
+GROUP BY month
+ORDER BY count DESC;
+
+SELECT 
+    COUNT(*) AS yahoo_users
+FROM users
+WHERE email LIKE '%yahoo.com';
+
+SELECT
+    CASE
+    	WHEN email LIKE '%gmail.com' THEN 'gmail'
+        WHEN email LIKE '%yahoo.com' THEN 'yahoo'
+        WHEN email LIKE '%hotmail.com' THEN 'hotmail'
+        ELSE 'other'
+    END AS provider,
+    COUNT(*) AS total_users
+FROM users
+GROUP BY provider
+ORDER BY total_users DESC;
